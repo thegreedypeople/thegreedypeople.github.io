5c3bba4506178e3ff744a6bc4e1b720316396e9d
thegreedypeople <thegreedypeople@users.noreply.github.com>
2023-03-05 19:45:43 +0800

    Add files via upload

--- ng/app1.js
+++ ng/app1.js
@@ -29,27 +29,7 @@ $scope.navs = [
 {nav:"Blog",link:"/blog"}
 ];
 
-// dream images
-$scope.list = "https://dream.ai/listing/";
-$scope.dreams = [
-{ 
-title: "Shallow Bay",
-link: "2f1d10cf-9413-4e89-b651-9c0dcb29a430"},
-{ 
-title: "Himaya",
-link: "7e644fe7-db9c-4490-ad29-0bdf194794c3"},
-{ 
-title: "The Greedy People",
-link: "da85a037-d036-4b20-a16d-fa8165a63170"},
-{ 
-title: "DOTFB: Descendants of the first born",
-link: "dbdb701b-a5b0-42e3-8557-325059b6c7d9"},
-{ 
-title:"dance with the devil",
-link:"8cd22744-a261-420e-aed2-47751d76d42b"},
-{
-title:"the witch of mercury",
-link:"84db3f58-85e7-42ba-8906-d93ebfb0be99"}];
+//$scope.random = Math.floor(Math.random() * 6);
 
 $scope.footyear = "03232015".slice(4)+' - '+new Date().getFullYear();
 
